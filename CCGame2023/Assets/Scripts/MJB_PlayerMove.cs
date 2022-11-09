34a2d7c491cb54aab62f6f904bae321c7cc0ddfd
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2022-11-08 23:37:18 -0500

    PlayerMove Script Organization
    -organized player move script a bit more
    -added comments for player move script

--- CCGame2023/Assets/Scripts/MJB_PlayerMove.cs
+++ CCGame2023/Assets/Scripts/MJB_PlayerMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MJB_PlayerMove : MonoBehaviour
 {
+    //variables for movement
     Rigidbody2D rb;
     public float jumpTime;
     private float jumpTimeCounter;
@@ -16,8 +17,8 @@ public class MJB_PlayerMove : MonoBehaviour
     [SerializeField] private int maxJumps;
     public float playerRotation;
 
+    //variables for animator
     public Animator myAnim;
-    public bool isWalking;
     public static MJB_PlayerMove instance;
 
     private void Awake() {
@@ -34,12 +35,15 @@ public class MJB_PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //player movement
         playerPos = transform.position;
 
         Vector2 velocity = rb.velocity;
         velocity.x = Input.GetAxis("Horizontal") * playerSpeed * (Time.deltaTime + 1);
         rb.velocity = velocity;
+        //sets playerSpeed parameter for animator
         myAnim.SetFloat("PlayerSpeed", Mathf.Abs(velocity.x));
+
 
         //flips character to face correct direction when walking
          if(Input.GetAxis("Horizontal") < 0)
@@ -53,24 +57,29 @@ public class MJB_PlayerMove : MonoBehaviour
         transform.rotation = Quaternion.Euler(0f, playerRotation, 0f);
 
 
-        RaycastHit2D ground = Physics2D.BoxCast(new Vector2(playerPos.x, playerPos.y -0.1f), new Vector2(0.95f, 0.02f), 0, -Vector2.up, 0.1f); //checking if player is touching ground using boxcast: BoxCast(origin, size, direction, distance)
+        //checks if player is touching ground using boxcast: BoxCast(origin, size, direction, distance)
+        RaycastHit2D ground = Physics2D.BoxCast(new Vector2(playerPos.x, playerPos.y -0.1f), new Vector2(0.95f, 0.02f), 0, -Vector2.up, 0.1f);
 
-        if(ground.collider != null) //double jump code
+
+        //grounded code (uses boxcast to determine if player is grounded)
+        if(ground.collider != null)
         {
             isGrounded = true;
             numJumps = maxJumps;
+
             myAnim.SetBool("isJumping", false);
             myAnim.SetBool("JumpPeaked", false);
         } else {
             isGrounded = false;
+            
             if(numJumps == maxJumps)
             {
                 numJumps--;
             }
         }
 
-        
-        if((Input.GetKeyDown("up")) && (isGrounded == true || numJumps > 0)) //jump code
+        //jump code (initial jump)
+        if((Input.GetKeyDown("up")) && (isGrounded == true || numJumps > 0))
         {
             isJumping = true;
             jumpTimeCounter = jumpTime;
@@ -86,7 +95,8 @@ public class MJB_PlayerMove : MonoBehaviour
             }
         }
 
-        if((Input.GetKey("up")) && isJumping == true) //extended jump code
+        //jump code (extended jump/holding down jump)
+        if((Input.GetKey("up")) && isJumping == true)
         {
             if(jumpTimeCounter > 0)
             {
@@ -103,8 +113,8 @@ public class MJB_PlayerMove : MonoBehaviour
             }
         }
 
-
-        if(Input.GetKeyUp("up")) //ends jump
+        //jump code (ends jump when lifting button/taking finger off button)
+        if(Input.GetKeyUp("up"))
         {
             isJumping = false;
             myAnim.SetBool("isJumping", false);
