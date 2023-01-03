9e6b1836598c672953b6cab1dddad03b88d603f0
FaceOfBanana <114019832+FaceOfBanana@users.noreply.github.com>
2023-01-03 14:28:28 -0500

    Basic changes

---
+++ CCGame2023/Assets/EricLevelEditor/scripts/LevelSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath);
+        FileInfo[] info = dir.GetFiles("*.*");
+        foreach(FileInfo i in info)
+        {
+            
+
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
