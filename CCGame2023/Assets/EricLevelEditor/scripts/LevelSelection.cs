84dfa48b15cbcd7f69df015a507965ec8db5b97c
FaceOfBanana <114019832+FaceOfBanana@users.noreply.github.com>
2023-01-05 14:30:56 -0500

    1/5 classwork
    worked on the level selection screen

--- CCGame2023/Assets/EricLevelEditor/scripts/LevelSelection.cs
+++ CCGame2023/Assets/EricLevelEditor/scripts/LevelSelection.cs
@@ -2,20 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 
 public class LevelSelection : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject levelPanel;
+    Vector3 newPanelPosition = new Vector3(-6f, 2.5f, 0f);
+    [SerializeField] RectTransform canvas;
+
     void Start()
     {
         DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath);
         FileInfo[] info = dir.GetFiles("*.*");
         foreach(FileInfo i in info)
         {
-            
-
-            
+            if(i.Name.Substring(i.Name.Length-4, 4)!="meta")
+            {
+                GameObject newPanel = Instantiate(levelPanel, newPanelPosition, Quaternion.identity);
+                newPanel.transform.parent = canvas;
+                if(newPanelPosition.y < 0f)
+                {
+                    newPanelPosition = new Vector3(newPanelPosition.x + 5f, 2.5f, 0f);
+                }
+                else newPanelPosition = new Vector3(newPanelPosition.x, -2f, 0f);
+                
+            }
         }
     }
 
