c374afde8a40ae295d4962f08391f15e435195c0
FaceOfBanana <114019832+FaceOfBanana@users.noreply.github.com>
2023-01-13 14:24:59 -0500

    Working on menu ui

--- CCGame2023/Assets/Scripts/MJB_SceneSwitch.cs
+++ CCGame2023/Assets/Scripts/MJB_SceneSwitch.cs
@@ -6,9 +6,21 @@ using UnityEngine.SceneManagement;
 public class MJB_SceneSwitch : MonoBehaviour
 {
     // Start is called before the first frame update
+    GameObject levelEditorMenu;
+    GameObject levelEditorMenuBackground;
+    
     void Start()
     {
-        
+        if(gameObject.name=="btnEditor")
+        {
+            print("Hi");
+            levelEditorMenu = GameObject.Find("levelEditorMenu");
+            levelEditorMenu.transform.position = new Vector3(0f, 200f, 0f);
+            levelEditorMenu.SetActive(false);
+            levelEditorMenuBackground = GameObject.Find("levelEditorMenuBackground");
+            levelEditorMenuBackground.transform.position = new Vector3(0f, 200f, 0f);
+            levelEditorMenuBackground.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -18,6 +30,17 @@ public class MJB_SceneSwitch : MonoBehaviour
     }
 
     public void moveToScene(int sceneID) {
-        SceneManager.LoadScene(sceneID);
+        if(sceneID!=100)
+        {
+            SceneManager.LoadScene(sceneID);
+        }
+        else
+        {
+            levelEditorMenu.SetActive(true);
+            levelEditorMenu.transform.position = new Vector3(0f, 0f, 0f);
+            levelEditorMenuBackground.SetActive(true);
+            levelEditorMenuBackground.transform.position = new Vector3(0f, 0f, 0f);
+
+        }
     }
 }
