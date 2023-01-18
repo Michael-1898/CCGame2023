01faddf03b54c758d60fa8e286f26102f172552e
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-01-18 14:08:07 -0500

    started adding 3rd enemy
    -need to have follow and run radius

---
+++ CCGame2023/Assets/Scripts/Enemy3/Enemy3Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3Controller : MonoBehaviour
+{
+    //movement variables
+    [SerializeField] float aggroRadius;
+    [SerializeField] float followRadius;
+    //[SerializeField] float runRadius;
+    [SerializeField] float moveSpeed;
+    [SerializeField] LayerMask playerLayer;
+    [SerializeField] Transform player;
+    bool aggroTaken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aggroTaken = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+
+        //initial aggro code (once aggro is taken, enemy will permanently be aggro to player no matter how far they are)
+        if(distanceFromPlayer < aggroRadius) {
+            aggroTaken = true;
+            aggroRadius = 900;
+        }
+
+
+        //if player is within aggro distance follow and outside of follow distance then follow (stops following if within follow radius)
+        if(distanceFromPlayer < aggroRadius && distanceFromPlayer > followRadius) {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, moveSpeed * Time.deltaTime);
+        } else if(Mathf.Round(distanceFromPlayer) == followRadius && distanceFromPlayer < aggroRadius) {
+            transform.position = transform.position;
+        } else if(distanceFromPlayer < followRadius) {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, -moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, aggroRadius);
+        Gizmos.DrawWireSphere(transform.position, followRadius);
+    }
+}
