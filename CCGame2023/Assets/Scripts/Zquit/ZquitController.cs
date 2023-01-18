3cd8ac1e53a3ea5168a24d812533c3b89a7da03c
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-01-17 19:53:09 -0500

    finished zquit enemy
    -toktok knockback started bugging out (need to fix)

--- CCGame2023/Assets/Scripts/Zquit/ZquitController.cs
+++ CCGame2023/Assets/Scripts/Zquit/ZquitController.cs
@@ -4,26 +4,120 @@ using UnityEngine;
 
 public class ZquitController : MonoBehaviour
 {
+    //movement variables
     [SerializeField] float aggroRadius;
     [SerializeField] float followRadius;
     [SerializeField] float moveSpeed;
-
     [SerializeField] LayerMask playerLayer;
     [SerializeField] Transform player;
+
+    //attk variables
+    [SerializeField] float attkCooldown;
+    float attkTimer;
+    bool isAttacking;
+    [SerializeField] float attkingSpeed;
+    bool aggroTaken;
+    Rigidbody2D rb;
+    [SerializeField] int enemyDmg;
+    bool playerHit;
+
+    //knockbacked variables
+    float hitTimer;
+    [SerializeField] float stunTime;
+    bool isKnocked;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isAttacking = false;
+        aggroTaken = false;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //linear drag when knockbacked
+        if(GetComponent<Health>().hit == true && rb.drag == 0) {
+            rb.drag = 1.5f;
+            isKnocked = true;
+        }
+
+        if(isKnocked) {
+            hitTimer += Time.deltaTime;
+        }
+
+        if(hitTimer >= stunTime) {
+            rb.drag = 0;
+            rb.velocity = Vector3.zero;
+            isKnocked = false;
+            hitTimer = 0;
+            if(isAttacking) {
+                isAttacking = false;
+            }
+        }
+
+
+
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
-        if(distanceFromPlayer < aggroRadius && distanceFromPlayer > followRadius) {
+
+        //initial aggro code (once aggro is taken, enemy will permanently be aggro to player no matter how far they are)
+        if(distanceFromPlayer < aggroRadius) {
+            aggroTaken = true;
+            aggroRadius = 900;
+        }
+
+        
+        //if player is within aggro distance follow and outside of follow distance then follow (stops following if within follow radius)
+        if(distanceFromPlayer < aggroRadius && distanceFromPlayer > followRadius && !isAttacking) {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, moveSpeed * Time.deltaTime);
         }
+
+
+        //Attk cooldown code
+        if(distanceFromPlayer < aggroRadius) {
+            if(!isAttacking) {
+            attkTimer += Time.deltaTime;
+            }
+
+            if(attkTimer >= attkCooldown) {
+                isAttacking = true;
+                attkTimer = 0;
+                Attack();
+            }
+        }
+
+
+        //attk stop code
+        if(isAttacking) {
+            if(distanceFromPlayer > followRadius + 10) {
+                rb.velocity = Vector3.zero;
+                isAttacking = false;
+            }
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if(col.gameObject.layer == 8) {
+            rb.velocity = Vector3.zero;
+            isAttacking = false;
+        }
+        if(col.gameObject.CompareTag("Player") && !playerHit) {
+            col.gameObject.GetComponent<PlayerHealth>().TakeDamage(enemyDmg);
+            playerHit = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if(playerHit) {
+            playerHit = false;
+        }
+    }
+
+    void Attack() {
+        Vector3 attkDir = (player.position - transform.position).normalized;
+        rb.velocity = attkDir * attkingSpeed * (Time.deltaTime + 1);
     }
 
     private void OnDrawGizmos() {
