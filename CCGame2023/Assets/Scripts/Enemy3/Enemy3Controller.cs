33a92c5b6244ab1266946eac3ddfa94a67c28b67
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-01-18 22:53:29 -0500

    movement 3rd enemy
    -finished movement and knockback for 3rd enemy
    -need to add attack for 3rd enemy and stop it from going through walls
    -since collision with player doesn't matter for this enemy, can switch to normal collider, not trigger, and just do ignore collision with player to stop collision with walls

--- CCGame2023/Assets/Scripts/Enemy3/Enemy3Controller.cs
+++ CCGame2023/Assets/Scripts/Enemy3/Enemy3Controller.cs
@@ -7,21 +7,47 @@ public class Enemy3Controller : MonoBehaviour
     //movement variables
     [SerializeField] float aggroRadius;
     [SerializeField] float followRadius;
-    //[SerializeField] float runRadius;
+    [SerializeField] float runRadius;
     [SerializeField] float moveSpeed;
     [SerializeField] LayerMask playerLayer;
     [SerializeField] Transform player;
     bool aggroTaken;
 
+
+    //knockbacked variables
+    float hitTimer;
+    [SerializeField] float stunTime;
+    bool isKnocked;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         aggroTaken = false;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //linear drag when knockbacked
+        if(GetComponent<Health>().hit == true && rb.drag == 0) {
+            rb.drag = 1.5f;
+            isKnocked = true;
+        }
+
+        if(isKnocked) {
+            hitTimer += Time.deltaTime;
+        }
+
+        if(hitTimer >= stunTime) {
+            rb.drag = 0;
+            rb.velocity = Vector3.zero;
+            isKnocked = false;
+            hitTimer = 0;
+        }
+
+
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
 
         //initial aggro code (once aggro is taken, enemy will permanently be aggro to player no matter how far they are)
@@ -34,9 +60,9 @@ public class Enemy3Controller : MonoBehaviour
         //if player is within aggro distance follow and outside of follow distance then follow (stops following if within follow radius)
         if(distanceFromPlayer < aggroRadius && distanceFromPlayer > followRadius) {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, moveSpeed * Time.deltaTime);
-        } else if(Mathf.Round(distanceFromPlayer) == followRadius && distanceFromPlayer < aggroRadius) {
+        } else if(distanceFromPlayer <= followRadius && distanceFromPlayer > runRadius) {
             transform.position = transform.position;
-        } else if(distanceFromPlayer < followRadius) {
+        } else if(distanceFromPlayer <= runRadius) {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, -moveSpeed * Time.deltaTime);
         }
     }
@@ -45,5 +71,6 @@ public class Enemy3Controller : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, aggroRadius);
         Gizmos.DrawWireSphere(transform.position, followRadius);
+        Gizmos.DrawWireSphere(transform.position, runRadius);
     }
 }
