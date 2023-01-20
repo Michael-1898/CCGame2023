826824e207554962f648632fddf8d09ab36313b6
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-01-20 00:24:26 -0500

    3rd enemy and tuning
    -finished 3rd enemy
    -tuned some values for other enemies and for 3rd enemy

--- CCGame2023/Assets/Scripts/Enemy3/Enemy3Controller.cs
+++ CCGame2023/Assets/Scripts/Enemy3/Enemy3Controller.cs
@@ -16,6 +16,9 @@ public class Enemy3Controller : MonoBehaviour
     //attk variables
     [SerializeField] int enemyDmg;
     [SerializeField] int enemyKnockback;
+    [SerializeField] float attkCooldown;
+    float attkTimer;
+    [SerializeField] GameObject bullet;
 
     //knockbacked variables
     float hitTimer;
@@ -63,10 +66,27 @@ public class Enemy3Controller : MonoBehaviour
         //if player is within aggro distance follow and outside of follow distance then follow (stops following if within follow radius)
         if(distanceFromPlayer < aggroRadius && distanceFromPlayer > followRadius) {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, moveSpeed * Time.deltaTime);
+            rb.velocity = Vector3.zero;
         } else if(distanceFromPlayer <= followRadius && distanceFromPlayer > runRadius) {
-            transform.position = transform.position;
+            if(transform.position.y <= player.position.y + 1.5) {
+                rb.velocity = Vector2.up * (moveSpeed/2) * (Time.deltaTime + 1);
+            } else {
+                rb.velocity = Vector3.zero;   
+            }
         } else if(distanceFromPlayer <= runRadius) {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, -moveSpeed * Time.deltaTime);
+            rb.velocity = Vector3.zero;
+        }
+
+
+        //Attk cooldown code
+        if(distanceFromPlayer < aggroRadius) {
+            attkTimer += Time.deltaTime;
+
+            if(attkTimer >= attkCooldown) {
+                attkTimer = 0;
+                Attack();
+            }
         }
     }
 
@@ -87,6 +107,11 @@ public class Enemy3Controller : MonoBehaviour
                 col.gameObject.GetComponent<Rigidbody2D>().velocity = -Vector2.right * enemyKnockback * (Time.deltaTime + 1);
             }
         }
+    }
+
+
+    void Attack() {
+        Instantiate(bullet, transform.position, Quaternion.identity);
     }
 
 
