37569cf9cf02ab5aeec13502b3b63b16d09e9778
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-02-06 00:20:22 -0500

    WIP toucan jumping added
    -need to work more on toucan jumping
    -need to add animation to toucan jumping
    -need to add ground check to toucan jumping

--- CCGame2023/Assets/Scripts/ToucanBoss/ToucanBoss.cs
+++ CCGame2023/Assets/Scripts/ToucanBoss/ToucanBoss.cs
@@ -6,17 +6,54 @@ public class ToucanBoss : MonoBehaviour
 {
     //fly variables
     [SerializeField] float minJumpCooldown;
-    [SerializeField] float jumpTimer;
+    float jumpTimer;
+    [SerializeField] float maxJumpCooldown;
+    [SerializeField] float jumpChance;
+    [SerializeField] float jumpStrength;
+    bool jump;
+
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        jumpTimer = minJumpCooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(jumpTimer == 0) {
+            jump = false;
+        }
+
+        jumpTimer += Time.deltaTime;
+        if(jumpTimer >= minJumpCooldown && !jump) {
+            Jump();
+        }
+    }
+
+    void Jump() {
+        //float tempJumpStrength;
+        float rand = Random.value;
+        if(rand <= jumpChance) {
+            jump = true;
+        }
+
+        if(jumpTimer >= maxJumpCooldown) {
+            jump = true;
+        }
+
+        if(jump) {
+            rb.velocity = Vector3.zero;
+            // if((jumpTimer - minJumpCooldown) <= (maxJumpCooldown - jumpTimer)) {
+            //     tempJumpStrength = jumpStrength + 0.5f;
+            // } else {
+            //     tempJumpStrength = jumpStrength - 1;
+            // }
+            rb.AddForce(transform.up * jumpStrength, ForceMode2D.Impulse);
+            jumpTimer = 0;
+        }
     }
 }
