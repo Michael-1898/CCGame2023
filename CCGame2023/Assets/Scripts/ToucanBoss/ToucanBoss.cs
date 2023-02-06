b6f697e97099bd1ea36693f10466e864f1508c10
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-02-05 23:22:54 -0500

    added toucan sprites

---
+++ CCGame2023/Assets/Scripts/ToucanBoss/ToucanBoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToucanBoss : MonoBehaviour
+{
+    //fly variables
+    [SerializeField] float minJumpCooldown;
+    [SerializeField] float jumpTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
