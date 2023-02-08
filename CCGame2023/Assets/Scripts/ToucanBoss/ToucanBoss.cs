8e7f3d1665439ea3f9b748d8da9370e36009704c
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-02-08 11:07:28 -0500

    rising and falling toucan
    worked on rising and falling for toucan

--- CCGame2023/Assets/Scripts/ToucanBoss/ToucanBoss.cs
+++ CCGame2023/Assets/Scripts/ToucanBoss/ToucanBoss.cs
@@ -15,12 +15,19 @@ public class ToucanBoss : MonoBehaviour
     [SerializeField] float maxJumpStrength;
     [SerializeField] float minJumpStrength;
     float jumpStrength;
+    Rigidbody2D rb;
+    bool isRising;
+    bool isDescending;
 
     //lateral movement variables
     [SerializeField] Transform player;
     bool isFacingRight;
 
-    Rigidbody2D rb;
+    //variables for groundCheck
+    bool isGrounded; //bool for ground check
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] GameObject groundCheck;
+    [SerializeField] float circleRadius;
 
     //variables for animator
     public Animator anim;
@@ -32,32 +39,53 @@ public class ToucanBoss : MonoBehaviour
         jumpTimer = minJumpCooldown;
         anim = GetComponent<Animator>();
         jumpStrength = minJumpStrength;
+        jumpCooldown = Random.Range(minJumpCooldown, maxJumpCooldown);
+        isDescending = false;
+        isRising = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //checks if player is touching ground using overlap circle
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, circleRadius, groundLayer);
+
+
+        //if toucan gets too high or far below player increase or decrease jumpCooldown
+        if(transform.position.y - player.position.y <= -5 && !isRising) {
+            jumpCooldown /= 2;
+            isRising = true;
+        } else if(isRising) {
+            isRising = false;
+        }
+        if(transform.position.y - player.position.y >= 9 && !isDescending) {
+            jumpCooldown *= 1.02f;
+            isDescending = true;
+        } else if(isDescending) {
+            isDescending = false;
+        }
+        Debug.Log("isRising: " + isRising);
+        Debug.Log("isDescending: " + isDescending);
+
+
+        //jump code animator
+        if(anim.GetBool("isJumping") && rb.velocity.y < 0) {
+            anim.SetBool("isJumping", false);
+        }
+        //jump cooldown and input code
+        jumpTimer += Time.deltaTime;
+        if(jumpTimer >= jumpCooldown) {
+            Jump();
+        } else if(isGrounded) {
+            Jump();
+        }
+
+
         //flipping to look at player
         if(transform.position.x < player.position.x && !isFacingRight) {
             Flip();
         } else if(transform.position.x > player.position.x && isFacingRight) {
             Flip();
-        }
-
-
-        if(anim.GetBool("isJumping") && rb.velocity.y < 0) {
-            anim.SetBool("isJumping", false);
-        }
-
-        // if(jumpTimer == 0) {
-        //     jump = false;
-        // }
-        
-
-        jumpTimer += Time.deltaTime;
-        jumpCooldown = Random.Range(minJumpCooldown, maxJumpCooldown);
-        if(jumpTimer >= jumpCooldown) {
-            Jump();
         }
     }
 
@@ -86,30 +114,43 @@ public class ToucanBoss : MonoBehaviour
         //     jumpTimer = 0;
         // }
 
-
     
 
         //if jumpCooldown is low do a small jump if high do a little 
-        float jumpCooldownDiff = maxJumpCooldown - minJumpCooldown;
-        float jumpStrengthDiff = maxJumpStrength - minJumpStrength;
-        if(jumpCooldown <= (minJumpCooldown + (jumpCooldownDiff/3))) {
-            jumpStrength = Random.Range(minJumpStrength, minJumpStrength + (jumpStrengthDiff/3));
-        } else if(jumpCooldown <= (minJumpCooldown + (jumpCooldownDiff * (2/3)))) {
-            jumpStrength = Random.Range(minJumpStrength + (jumpStrengthDiff/3), minJumpStrength + (jumpStrengthDiff * (2/3)));
-        } else if(jumpCooldown <= maxJumpCooldown) {
-            jumpStrength = Random.Range(minJumpStrength + (jumpStrengthDiff * (2/3)), maxJumpStrength);
-        }
-        // Debug.Log(jumpCooldown);
-        // Debug.Log(jumpStrength);
+        if(!isGrounded) {
+            float jumpCooldownDiff = maxJumpCooldown - minJumpCooldown;
+            float jumpStrengthDiff = maxJumpStrength - minJumpStrength;
+            if(jumpCooldown <= (minJumpCooldown + (jumpCooldownDiff/3))) {
+                jumpStrength = Random.Range(minJumpStrength, minJumpStrength + (jumpStrengthDiff/3));
+            } else if(jumpCooldown <= (minJumpCooldown + (jumpCooldownDiff * (2/3)))) {
+                jumpStrength = Random.Range(minJumpStrength + (jumpStrengthDiff/3), minJumpStrength + (jumpStrengthDiff * (2/3)));
+            } else if(jumpCooldown <= maxJumpCooldown) {
+                jumpStrength = Random.Range(minJumpStrength + (jumpStrengthDiff * (2/3)), maxJumpStrength);
+            }
 
-        rb.velocity = Vector3.zero;
-        rb.AddForce(transform.up * jumpStrength, ForceMode2D.Impulse);
-        anim.SetBool("isJumping", true);
-        jumpTimer = 0;
+            //actual jump movement code
+            rb.velocity = Vector3.zero;
+            rb.AddForce(transform.up * jumpStrength, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+            jumpTimer = 0;
+        } else if(isGrounded) {
+            rb.velocity = Vector3.zero;
+            rb.AddForce(transform.up * jumpStrength, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+            jumpTimer = 0;
+        }
+        //new jump cooldown
+        jumpCooldown = Random.Range(minJumpCooldown, maxJumpCooldown);
+        
     }
 
     void Flip() {
         isFacingRight = !isFacingRight;
         transform.Rotate(new Vector3(0, 180, 0));
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.transform.position, circleRadius);
     }
 }
