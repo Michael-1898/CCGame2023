0529a6a86f91ffcfd13291a450193492e808733f
FaceOfBanana <114019832+FaceOfBanana@users.noreply.github.com>
2023-02-28 14:28:18 -0500

    WISARGO
    and other stuff :8ball:

---
+++ CCGame2023/Assets/Scripts/Wizardo/WizardoAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardoAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            Attack();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    {
+        int random = Mathf.FloorToInt(Random.Range(1,5));
+        print(random);
+    }
+}
