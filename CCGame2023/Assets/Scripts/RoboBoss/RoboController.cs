0b7f08a5e819bc0f0e57435f681b9c59f90cf4b8
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-03-07 14:32:13 -0500

    robo boss
    -working on 1st attk for robo boss

--- CCGame2023/Assets/Scripts/RoboBoss/RoboController.cs
+++ CCGame2023/Assets/Scripts/RoboBoss/RoboController.cs
@@ -9,6 +9,16 @@ public class RoboController : MonoBehaviour
     [SerializeField] float[] attkPhaseLengths;
     int currentPhase;
 
+    //variables for attk1
+    float canonShotTimer;
+    [SerializeField] float canonShotCooldown;
+    [SerializeField] GameObject[] canonBalls;
+    [SerializeField] Transform canonPoint;
+    [SerializeField] Transform canonPivot;
+    bool canonShooting;
+    float canonAngle;
+    [SerializeField] float canonRotateSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,15 +50,35 @@ public class RoboController : MonoBehaviour
         }
     }
 
-    void Attk1() {
-        print("1");
+    void Attk1() { //canon shooting
+        canonShotTimer += Time.deltaTime;
+    
+        if(canonShotTimer >= canonShotCooldown && !canonShooting) {
+            canonAngle = Random.Range(-150, 26);
+            
+            //sets rotate speed to rotate towards angle from correct direction
+            if(canonPivot.rotation.z < canonAngle && canonRotateSpeed < 0) {
+                canonRotateSpeed *= -1;
+            }
+            if(canonPivot.rotation.z > canonAngle && canonRotateSpeed > 0) {
+                canonRotateSpeed *= -1;
+            }
+            canonShooting = true;
+        }
+
+        if(canonShooting && Mathf.Round(canonPivot.rotation.z) != canonAngle) { //if not at correct angle yet
+            canonPivot.rotation = Quaternion.Euler(0f, 0f, canonRotateSpeed * Time.deltaTime); //rotate towards correct angle
+        } else if(Mathf.Round(canonPivot.rotation.z) == canonAngle) { //if at correct angle
+            Instantiate(canonBalls[Random.Range(0,3)], canonPoint.position, Quaternion.identity);
+            canonShotTimer = 0;
+        }
     }
 
-    void Attk2() {
+    void Attk2() { //ramming
         print("2");
     }
 
-    void Attk3() {
+    void Attk3() { //missile shooting
         print("3");
     }
 }
